704acc6e88e867bbff7a063e61fe3863782b9fbd
Ryan Tick <rtick@nd.edu>
2015-10-15 13:16:06 -0400

    This is my final submission.

--- run.rb
+++ run.rb
@@ -8,13 +8,45 @@ get '/' do
 end
 
 get '/form' do
-  # code!
-  erb :form, layout: :main
+	# code!
+	@states = [] #create an array called states
+
+	#create a hash
+	key = ["PA","NY","TX","NC","SC"] #key
+	value = ["Pennsylvania", "New York", "Texas", "North Carolina", "South Carolina"] #value
+
+
+	0.upto(4) do | s | #going to add each hash to our array
+		state = {} #initializing state hash
+		state[:id] = key[s] #adding ID
+		state[:name] = value[s] #adding NAME
+		@states << state #adding state hash to states array
+	end 
+
+	@states.sort_by!{|state| state[:name]} #sorted alphabetically
+	@params = {}
+	erb :form, layout: :main
 end
 
 post '/form' do
-  # code!
-  erb :form, layout: :main
+	# code!
+	@states = [] #create an array called states
+
+	#create a hash
+	key = ["PA","NY","TX","NC","SC"] #key
+	value = ["Pennsylvania", "New York", "Texas", "North Carolina", "South Carolina"] #value
+
+
+	0.upto(4) do | s | #going to add each hash to our array
+		state = {} #initializing state hash
+		state[:id] = key[s] #adding ID
+		state[:name] = value[s] #adding NAME
+		@states << state #adding state hash to states array
+	end 
+
+	@states.sort_by!{|state| state[:name]} #sorted alphabetically
+
+	erb :form, layout: :main
 end
 
 get '/example' do
